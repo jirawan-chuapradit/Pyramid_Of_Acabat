7a27a88feef176eb28d309e18635ee2dd289ca90
Jirawan Chuapradit <jirawan-chuapradit@users.noreply.github.com>
2017-11-17 22:05:19 +0700

    Make PlayButton
    Updated to Beer

---
+++ project3/core/src/Sprites/PinkBlock.java
@@ -0,0 +1,43 @@
+package Sprites;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.game.Pyramid;
+
+public class PinkBlock extends InteractiveTileObject {
+	
+	private Fixture fixture;
+	
+	public PinkBlock (World world, TiledMap map, Rectangle bounds) {
+		super(world, map, bounds);
+		BodyDef bdef = new BodyDef();
+		FixtureDef fdef = new FixtureDef();
+		PolygonShape shape = new PolygonShape();
+		
+		bdef.type = BodyDef.BodyType.StaticBody;
+		bdef.position.set((bounds.getX() + bounds.getWidth() / 2) / Pyramid.PPM, (bounds.getY() + bounds.getHeight() / 2) / Pyramid.PPM);
+		
+		body = world.createBody(bdef);
+		
+		shape.setAsBox(bounds.getWidth() / 2 / Pyramid.PPM, bounds.getHeight() / 2 / Pyramid.PPM);
+		fdef.shape = shape;
+		fdef.filter.groupIndex = -1;
+		fixture = body.createFixture(fdef);
+	}
+	
+	
+	public void switchState() {
+		if(fixture.isSensor()) {
+			fixture.setSensor(false);
+		}
+		else { 
+			fixture.setSensor(true);
+		}
+	}
+
+}
--- project3/core/src/com/mygdx/game/Pyramid.java
+++ project3/core/src/com/mygdx/game/Pyramid.java
@@ -1,13 +1,15 @@
 package com.mygdx.game;
 
-import com.Screens.PlayScreen;
-import com.badlogic.gdx.ApplicationListener;
+
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.handles.Content;
 
-public class Pyramid extends Game implements ApplicationListener {
+import State.Menu;
+import handles.Content;
+
+public class Pyramid extends Game {
 	
 	public static final int V_WIDTH = 1280;
 	public static final int V_HEIGHT = 720;
@@ -15,15 +17,22 @@ public class Pyramid extends Game implements ApplicationListener {
 	
 	public SpriteBatch sb;
 	
+	private Texture img;
+	
 	public static Content res;
+	public Pyramid() {
+		
+	}
+	
 	
 	@Override
 	public void create () {
 		
 		sb = new SpriteBatch();
-		setScreen(new PlayScreen(this));
+		Gdx.gl.glClearColor(0, 1, 0, 1);
+		this.setScreen(new Menu(this));
 		
-		res = new Content();
+
 		
 		
 	}
@@ -31,10 +40,14 @@ public class Pyramid extends Game implements ApplicationListener {
 	@Override
 	public void render () {
 		super.render();
+		
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 	}
 	
 	@Override
 	public void dispose () {
+
+		sb.dispose();
 
 	}
 	
